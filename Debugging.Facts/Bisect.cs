9e84d27595124c5392b6478b9ec282fc1bffbb0b
Philipp Dolder <philipp.dolder@gmail.com>
2017-03-21 15:34:01 +0100

    Add bisect baseline

---
+++ Debugging.Facts/Bisect.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Debugging
+{
+    public class Bisect
+    {
+        [Fact]
+        public void AFeatureThatIsWorking()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
